772818b87a255489944497c0e35e2e83d994f54e
Stuart Kent <skent.dev@gmail.com>
2019-12-04 08:39:49 -0500

    day04: nasty fasty solution

--- day04/src/main/kotlin/Main.kt
+++ day04/src/main/kotlin/Main.kt
@@ -1,3 +1,67 @@
-fun main() {
+import extensions.allMatch
+import extensions.digits
 
+fun main() {
+//    // Part 1
+//    (273025..767253)
+//        .map(Int::digits)
+//        .filter { digits ->
+//            digits
+//                .windowed(size = 2, step = 1)
+//                .any { pair -> pair.allMatch() }
+//        }
+//        .filter(Iterable<Int>::isNonDecreasing)
+//        .count()
+//        .also { println(it) }
+
+    println(111122.digits().runs())
+    println(112233.digits().runs())
+    println(221133.digits().runs())
+
+    // Part 2
+    (273025..767253)
+        .map(Int::digits)
+        .filter { digits ->
+            digits
+                .runs()
+                .any { it.size == 2 }
+        }
+        .filter(Iterable<Int>::isNonDecreasing)
+        .count()
+        .also { println(it) }
+}
+
+fun <T : Comparable<T>> Iterable<T>.isNonDecreasing(): Boolean {
+    return windowed(size = 2, step = 1)
+        .all { pair -> pair[0] <= pair[1] }
+}
+
+fun <T: Comparable<T>> Iterable<T>.runs(): List<List<T>> {
+    val allRuns = mutableListOf<List<T>>()
+    var currentValue: T? = null
+    var currentRunLength = 0
+
+    val iterator = iterator()
+    while (iterator.hasNext()) {
+        val nextValue = iterator.next()
+
+        if (currentRunLength == 0) {
+            currentValue = nextValue
+            currentRunLength = 1
+        } else {
+            if (nextValue == currentValue) {
+                currentRunLength += 1
+            } else {
+                allRuns.add(List(currentRunLength) { currentValue!! })
+                currentValue = nextValue
+                currentRunLength = 1
+            }
+        }
+    }
+
+    if (currentRunLength > 0) {
+        allRuns.add(List(currentRunLength) { currentValue!! })
+    }
+
+    return allRuns
 }
