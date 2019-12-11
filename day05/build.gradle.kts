943c01183ae742c73093fd44f234e44ee4013515
Stuart Kent <skent.dev@gmail.com>
2019-12-11 18:17:47 -0500

    Remove test config from days without tests

--- day05/build.gradle.kts
+++ day05/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
-
 plugins {
     kotlin("jvm")
     application
@@ -13,28 +11,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":utilities"))
     implementation(project(":intcode"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
-    }
-
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    test {
-        useJUnitPlatform()
-        testLogging.events = setOf(
-            PASSED,
-            FAILED,
-            SKIPPED,
-            STANDARD_OUT,
-            STANDARD_ERROR
-        )
     }
 }
 
--- day11/build.gradle.kts
+++ day11/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
-
 plugins {
     kotlin("jvm")
     application
