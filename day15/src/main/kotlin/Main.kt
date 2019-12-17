2a33b594c9d500528f1933b6e33912dcd3201903
Stuart Kent <skent.dev@gmail.com>
2019-12-17 18:44:11 -0500

    day15: wip

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
+import GridDirection.*
 import ShipAreaType.*
-import java.lang.IllegalStateException
-import java.util.*
 
 fun main() {
     val inputProgram = resourceFile("input.txt")
@@ -9,39 +8,38 @@ fun main() {
         .split(',')
         .map(String::toLong)
 
-    val computer = Computer()
-    var droidLocation = GridPoint2d.origin
-    var droidDirection = GridDirection.N
-    val ship = mutableMapOf<GridPoint2d, ShipAreaType>().withDefault { Unknown }.apply {
-        put(droidLocation, Floor)
-    }
+    val shipMap = computeShipMap(program = inputProgram)
+}
 
-    computer.execute(
-        program = inputProgram,
-        source = source@{
-            val stdInput = Scanner(System.`in`).nextLong()
+fun computeShipMap(program: List<Long>): Map<GridPoint2d, ShipAreaType> {
+    var droidCoords = GridPoint2d.origin
+    val droidPath = mutableListOf<GridDirection>()
+    val shipMap = mutableMapOf<GridPoint2d, ShipAreaType>()
 
-            droidDirection = when (stdInput) {
-                1L -> GridDirection.N
-                2L -> GridDirection.S
-                3L -> GridDirection.W
-                4L -> GridDirection.E
-                else -> throw IllegalStateException("Invalid input received")
-            }
+    Computer().execute(
+        program = program,
+        source = source@ {
+            val nextStep = GridDirection.values()
+                .firstOrNull { droidCoords + it.toUnitVector() !in shipMap }
 
-            return@source stdInput
+            return@source (nextStep?.let {
+                // we have found a location that's not in the map yet; try moving there
+                nextStep
+            } ?: run {
+                droidPath.last().`180`()
+            }).asInput()
         },
         sink = { output ->
-            val shipAreaType = values()[output.toInt()]
-            val targetLocation = droidLocation + droidDirection.toUnitVector()
-            ship += targetLocation to shipAreaType
-            if (shipAreaType != Wall) droidLocation = targetLocation
-            displayShip(ship, droidLocation)
+//            val shipAreaType = values()[output.toInt()]
+//            val targetLocation = droidCoords + nextStep().toUnitVector()
+//            ship += targetLocation to shipAreaType
+//            if (shipAreaType != Wall) droidLocationData = targetLocation
+//            displayShip(ship, droidLocationData)
         }
     )
 }
 
-fun displayShip(ship: Map<GridPoint2d, ShipAreaType>, droidLocation: GridPoint2d) {
+private fun printShipMap(ship: Map<GridPoint2d, ShipAreaType>) {
     val (left, right, bottom, top) = ship.keys.bounds()
 
     for (y in top downTo bottom) {
@@ -50,7 +48,6 @@ fun displayShip(ship: Map<GridPoint2d, ShipAreaType>, droidLocation: GridPoint2d
 
             print(
                 when (location) {
-                    droidLocation -> 'D'
                     GridPoint2d.origin -> '*'
                     else -> {
                         when (ship.getValue(GridPoint2d(x, y))) {
@@ -70,4 +67,13 @@ fun displayShip(ship: Map<GridPoint2d, ShipAreaType>, droidLocation: GridPoint2d
 
 enum class ShipAreaType {
     Wall, Floor, OxygenSystem, Unknown
+}
+
+private data class Location(val coords: GridPoint2d, val steps: List<GridDirection>)
+
+fun GridDirection.asInput() = when (this) {
+    N -> 1L
+    E -> 4L
+    S -> 2L
+    W -> 3L
 }
