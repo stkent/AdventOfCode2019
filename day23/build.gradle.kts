aaa0b610bd27e34b37239c713d503ff3db65a77f
Stuart Kent <skent.dev@gmail.com>
2019-12-24 15:44:09 -0500

    Update build scripts for days 21, 23

--- day23/build.gradle.kts
+++ day23/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
-
 plugins {
     kotlin("jvm")
     application
@@ -12,28 +10,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":utilities"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+    implementation(project(":intcode"))
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
-    }
-
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    test {
-        useJUnitPlatform()
-        testLogging.events = setOf(
-            PASSED,
-            FAILED,
-            SKIPPED,
-            STANDARD_OUT,
-            STANDARD_ERROR
-        )
     }
 }
 
